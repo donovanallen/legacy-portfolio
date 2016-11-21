b12524726fc26e37332320f7e9aa8b4b081421d8
Donovan Allen <dallen.928@gmail.com>
2016-11-21 13:26:13 -0500

    basic template master, no info, before fullpage

--- script.js
+++ script.js
@@ -3,4 +3,36 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+
+
+
+  //   var h1 = $("h1");
+  //
+  //   h1.on("hover", function(){
+  //     $(this).append("web dev");
+  //   })
+  //
+  //   $(window).scroll(function() {
+  //     if ($(this).scrollTop() > 1){
+  //     $('header').addClass("sticky");
+  //   } else{
+  //     $('header').removeClass("sticky");
+  //   }
+  // });
+  //
+  //   $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 30){
+  //     $('header').addClass("sticky");
+  //     $("h1").text("nda");
+  //     $("h3").hide();  // display: block/none
+  //   }
+  //   else{
+  //     $('header').removeClass("sticky");
+  //     $("h1").show("donovan allen")
+  //   }
+  //   });
 });
